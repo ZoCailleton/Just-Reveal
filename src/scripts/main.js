e3f8462953c14140bc6f06f6cffc69be59835d30
Enzo Cailleton <49985085+ZoCailleton@users.noreply.github.com>
2022-11-13 19:01:50 +0100

    Feat: Setup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,8 @@ let scene = null;
 let camera = null;
 let renderer = null;
 let canvas = null;
+let ambiantLight = null;
+let boardMesh = null;
 
 const sizes = {
   width: 0,
@@ -24,25 +26,58 @@ const updateSizes = () => {
 window.addEventListener('resize', updateSizes);
 
 const setupCanvas = () => {
+
   canvas = document.getElementById('webgl');
-  canvas.style.backgroundColor = 'red';
   updateSizes();
+  
 }
 
 const setupScene = () => {
+
   scene = new THREE.Scene();
+
   camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height);
+  scene.add(camera);
+
+  renderer = new THREE.WebGLRenderer();
+
+}
+
+const setupLights = () => {
+
+  ambiantLight = new THREE.AmbientLight(0xffffff);
+  scene.add(ambiantLight);
+
+}
+
+const setupWorld = () => {
+
+  let geo = new THREE.BoxGeometry(2, 2, 2);
+
+  let mat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
+  });
+  
+  boardMesh = new THREE.Mesh(geo, mat);
+  scene.add(boardMesh);
+
 }
 
 const tick = () => {
+
+  renderer.render(scene, camera);
   
   requestAnimationFrame(tick);
+
 }
 
 const startExperience = () => {
 
   setupCanvas();
   setupScene();
+  setupLights();
+  setupWorld();
 
   tick();
 
